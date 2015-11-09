5679d345581d300eac88b3ac7704de5754e74d5a
Tyler Ackerson <tackersonMSU12@gmail.com>
2015-11-09 09:32:49 -0800

    Complets LRU cache

--- lib/p06_lru_cache.rb
+++ lib/p06_lru_cache.rb
@@ -15,6 +15,13 @@ class LRUCache
   end
 
   def get(key)
+    if @map[key]
+      link = @map[key]
+      update_link!(link)
+      link.val
+    else
+      calc!(key)
+    end
   end
 
   def to_s
@@ -24,13 +31,29 @@ class LRUCache
   private
 
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    new_link = @store.insert(key, val)
+    @map[key] = new_link
+
+    eject! if count > @max
+    val
   end
 
   def update_link!(link)
-    # suggested helper method; move a link to the end of the list
+    link.prev.next = link.next
+    link.next.prev = link.prev
+
+    link.prev = @store.tail.prev
+    @store.tail.prev.next = link
+    link.next = @store.tail
+    @store.tail.prev = link
   end
 
   def eject!
+    remove_link = @store.head.next
+    remove_link.prev.next = remove_link.next
+    remove_link.next.prev = remove_link.prev
+    @map.delete(remove_link.key)
+    nil
   end
 end
